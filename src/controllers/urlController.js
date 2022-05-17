efa49ff2a993127068077407cab139e4f0e64045
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-17 15:07:38 +0530

    Get Api

---
+++ src/controllers/urlController.js
@@ -0,0 +1,11 @@
+const urlModel = require("../models/urlModel");
+
+
+const getUrl = async function (req, res) {
+    try {
+        // const 
+    }
+    catch (err) {
+        res.status(500).send({ status: false, message: err.message })
+    }
+}
